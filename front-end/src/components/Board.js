9d9450110da1dfbbe4cb12a82007208a01c3ac90
drakenevadie19 <drakennievadje19@gmail.com>
2024-03-23 03:25:26 -0500

    New Change: Adding function to clear and blocking any further action when one player won

--- front-end/src/components/Board.js
+++ front-end/src/components/Board.js
@@ -28,6 +28,13 @@ const Board = () => {
         // alert(`square ${i} is clicked`);
         // 1. Make a copy of Squares state array
         const newSquare = [...square];
+        
+        const winnerDeclared = Boolean(calculateWinner(newSquare));
+        const squareFilled = Boolean(newSquare[i]);
+        if (winnerDeclared || squareFilled) {
+            return;
+        }
+
         // 2. Mutate the copy, settin gthe ith element to 'X'
         newSquare[i] = xIsNext ? 'X' : 'O';
         // 3. Call the setSquares function with the mutated copy
@@ -47,6 +54,10 @@ const Board = () => {
     ? `Winner: ${winner}`
     : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
+    const clear = () => {
+        setSquare(Array(9).fill(null));
+    }
+
     return (
         <div className="board">
             <div className="status">{status}</div>
@@ -58,6 +69,10 @@ const Board = () => {
             </div>
             <div className="rowOfThree">
                 {renderSquare(6)} {renderSquare(7)} {renderSquare(8)}
+            </div>
+
+            <div>
+                <button onClick={clear}>Clear Matrix</button>
             </div>
         </div>
     );
