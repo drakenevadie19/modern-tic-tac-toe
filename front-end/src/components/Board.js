43565526b1142646fc6f9cff42f8c306252c017b
drakenevadie19 <drakennievadje19@gmail.com>
2024-03-23 13:53:54 -0500

    New Change: Small Change on the function to calculate winner

--- front-end/src/components/Board.js
+++ front-end/src/components/Board.js
@@ -5,7 +5,7 @@ function calculateWinner(squares) {
     const lines = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],  // rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // colummns
-        [0, 4, 8], [1, 4, 7] // diagonals
+        [0, 4, 8], [2, 4, 6] // diagonals
     ]
 
     for (let line of lines) {
