43ab1260c7394f78a2035754b4c00a04c0ffc007
drakenevadie19 <drakennievadje19@gmail.com>
2024-04-24 10:12:35 -0500

    New Change: Successfully add function to fetch data and display team's name, and gather them into arrays => works on this arrays

--- front-end/src/components/fetch-data/team-data.js
+++ front-end/src/components/fetch-data/team-data.js
@@ -1,63 +1,18 @@
-// import { useState, useEffect } from 'react';
-// import leagueData from './league-data';
-// import axios from 'axios';
-
-// // Can only choose team from 2010-2023
-// const TeamData = () => {
-//     const [teamId, setTeamId] = useState([]);
-//     const [teamName, setTeamName] = useState([]);
-//     const [teamLogo, setTeamLogo] = useState([]);
-//     const [current, setCurrent] = useState(null);
-
-//     const axios  = require('axios');
-
-//     const options = {
-//         method: 'GET',
-//         url: 'https://api-football-v1.p.rapidapi.com/v3/teams',
-//         params: {
-//             league: '78',
-//             season: '2023'
-//         },
-//         headers: {
-//             'X-RapidAPI-Key': '517c480401msha0c78d26c8b7bbfp1bd1bejsnfd252439e7aa',
-//             'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-//         }
-//     };
-
-//     try {
-//         const response = axios(options); // Use axios(options) for GET requests
-//         console.log(response.data);
-//         setCurrent(response.data.json); // Assuming "json" is the key in the response data
-//       } catch (error) {
-//         console.error(error);
-//       }
-
-//     return (
-//         <>
-//             <p>{current}</p>
-//         </>
-//     )
-// }
-
-// export default TeamData;
-
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const TeamData = () => {
+    const [allTeamInFiveLeagues, setAllTeamInFiveLeagues] = useState([]);
     const [teamProfile, setTeamProfile] = useState([]);
-    const [teamId, setTeamId] = useState([]);
-    const [teamName, setTeamName] = useState([]);
-    const [teamLogo, setTeamLogo] = useState([]);
     const [current, setCurrent] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (leagueNumb, seasonNumb) => {
             try {
                 const response = await axios.get('https://api-football-v1.p.rapidapi.com/v3/teams', {
                     params: {
-                        league: '78',
-                        season: '2023'
+                        league: leagueNumb,
+                        season: seasonNumb
                     },
                     headers: {
                         'X-RapidAPI-Key': '517c480401msha0c78d26c8b7bbfp1bd1bejsnfd252439e7aa',
@@ -66,20 +21,21 @@ const TeamData = () => {
                 });
                 // console.log(response.data.response);
                 setCurrent(response.data.response);
+                current.map((team) => {
+                    teamProfile.push({
+                        id: team.team.id,
+                        name: team.team.name,
+                        logo: team.team.logo
+                    })
+                })
             } catch (error) {
                 console.error(error);
             }
         };
-        fetchData();
-    }, []); // Empty array as dependency to run only once on component mount
+        fetchData(135, 2023);
+    }); // Empty array as dependency to run only once on component mount
 
-    current.map((team) => {
-        teamProfile.push({
-            id: team.team.id,
-            name: team.team.name,
-            logo: team.team.logo
-        })
-    })
+    
     
     console.log(teamProfile);
 
